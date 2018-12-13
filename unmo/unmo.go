511c19cc860df0de8e537d50da3656c67e262f7d
Katsuyuki Nakamura <katsuyuki.nakamura@gmail.com>
2018-12-13 16:44:32 +0900

    CHAPTER 4 - GUI

--- unmo/unmo.go
+++ unmo/unmo.go
@@ -1,24 +1,30 @@
 package unmo
 
 import (
+	"math/rand"
+
 	"github.com/katsuyuki-nakamura/unmo/responder"
 	randomresponder "github.com/katsuyuki-nakamura/unmo/responder/random"
+	whatresponder "github.com/katsuyuki-nakamura/unmo/responder/what"
 )
 
 type Unmo struct {
-	name      string
-	responder responder.Responder
+	name       string
+	responders []responder.Responder
+	responder  responder.Responder
 }
 
 func NewUnmo(name string) *Unmo {
-	u := new(Unmo)
-	u.name = name
-	//	u.responder = whatresponder.NewWhatResponder()
-	u.responder = randomresponder.NewRandomResponder()
+	u := &(Unmo{
+		name:       name,
+		responders: []responder.Responder{whatresponder.NewWhatResponder(), randomresponder.NewRandomResponder()},
+	})
+	u.responder = u.responders[0]
 	return u
 }
 
 func (u Unmo) Dialogue(input string) string {
+	u.responder = u.responders[rand.Intn(2)]
 	return u.responder.Response(input)
 }
 
