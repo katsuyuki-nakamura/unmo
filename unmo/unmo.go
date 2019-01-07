4b7b11c21e8b86e1dfc3904d07572c2c51501e0b
Katsuyuki Nakamura <katsuyuki.nakamura@gmail.com>
2019-01-07 12:05:15 +0900

    5-4 cond

--- unmo/unmo.go
+++ unmo/unmo.go
@@ -10,24 +10,34 @@ import (
 )
 
 type Unmo struct {
-	name       string
-	responders []responder.Responder
-	responder  responder.Responder
+	name              string
+	responder_what    responder.Responder
+	responder_random  responder.Responder
+	responder_pattern responder.Responder
+	responder         responder.Responder
 }
 
 func NewUnmo(name string) *Unmo {
 	u := new(Unmo)
 	u.name = name
-	u.responders = append(u.responders, whatresponder.NewWhatResponder())
-	u.responders = append(u.responders, randomresponder.NewRandomResponder())
-	u.responders = append(u.responders, patternresponder.NewPatternResponder())
-	u.responder = u.responders[0]
+	u.responder_what = whatresponder.NewWhatResponder()
+	u.responder_random = randomresponder.NewRandomResponder()
+	u.responder_pattern = patternresponder.NewPatternResponder()
+	u.responder = u.responder_pattern
 	return u
 }
 
 // 構造体のメンバーを変更する場合は、ポインタ渡し
 func (u *Unmo) Dialogue(input string) string {
-	u.responder = u.responders[rand.Intn(len(u.responders))]
+	num := rand.Intn(100)
+	switch {
+	case (num >= 0 && num < 60):
+		u.responder = u.responder_what
+	case (num > 60 && num < 90):
+		u.responder = u.responder_random
+	default:
+		u.responder = u.responder_pattern
+	}
 	return u.responder.Response(input)
 }
 
